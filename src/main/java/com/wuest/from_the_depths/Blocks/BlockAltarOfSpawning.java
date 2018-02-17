71ccf0e676a4b15cd6d03f6fc61f7ad985b35456
Brian-Wuest <Brian.Wuest@outlook.com>
2018-02-17 09:34:10 -0600

    Adding model for Altar.

--- src/main/java/com/wuest/from_the_depths/Blocks/BlockAltarOfSpawning.java
+++ src/main/java/com/wuest/from_the_depths/Blocks/BlockAltarOfSpawning.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Path;
+import java.util.Random;
 
 import com.google.common.base.Strings;
 import com.google.common.io.Files;
@@ -16,6 +17,7 @@ import com.wuest.from_the_depths.TileEntities.TileEntityAltarOfSpawning;
 import jline.internal.Log.Level;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
@@ -29,13 +31,19 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.JsonToNBT;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTUtil;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.FMLLog;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * 
@@ -45,12 +53,17 @@ import net.minecraftforge.fml.common.FMLLog;
 public class BlockAltarOfSpawning extends TileBlockBase<TileEntityAltarOfSpawning>
 {
 	/**
+	 * The see through material for this block.
+	 */
+	public static SeeThroughMaterial BlockMaterial = new SeeThroughMaterial(MapColor.AIR).setTranslucent(true);
+	
+	/**
 	 * Initializes a new instance of the BlockAlterOfSpawning class.
 	 * @param name The name to register this block as.
 	 */
 	public BlockAltarOfSpawning(String name)
 	{
-		super(Material.ROCK);
+		super(BlockMaterial);
 		this.setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
 		this.setSoundType(SoundType.STONE);
 		this.setBlockUnbreakable();
@@ -64,6 +77,49 @@ public class BlockAltarOfSpawning extends TileBlockBase<TileEntityAltarOfSpawnin
 	{
 		return 20;
 	}
+	
+    /**
+     * Used to determine ambient occlusion and culling when rebuilding chunks for render
+     */
+    @Override
+	public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+    
+	/**
+	 * Determines if a torch can be placed on the top surface of this block.
+	 * Useful for creating your own block that torches can be on, such as fences.
+	 *
+	 * @param state The current state
+	 * @param world The current world
+	 * @param pos Block position in world
+	 * @return True to allow the torch to be placed
+	 */
+	@Override
+	public boolean canPlaceTorchOnTop(IBlockState state, IBlockAccess world, BlockPos pos)
+	{
+		return false;
+	}
+	
+	@Override
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
+	
+    @SideOnly(Side.CLIENT)
+    @Override
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.CUTOUT;
+    }
+    
+    @Override
+	public EnumBlockRenderType getRenderType(IBlockState state)
+    {
+        return EnumBlockRenderType.MODEL;
+    }
 	
 	/**
 	 * Determines if this block can provide power.
@@ -86,28 +142,6 @@ public class BlockAltarOfSpawning extends TileBlockBase<TileEntityAltarOfSpawnin
     		ItemStack usedItem = playerIn.getHeldItem(hand);
     		
     		ItemStack offHand = playerIn.getHeldItem(EnumHand.OFF_HAND);
-    		
-//    		if (offHand.getItem() == Items.SPAWN_EGG)
-//    		{
-//    			ItemMonsterPlacer spawnEgg = (ItemMonsterPlacer)offHand.getItem();
-//    			ResourceLocation testMonster = ItemMonsterPlacer.getNamedIdFrom(offHand);
-//    			
-//    			Entity entity = EntityList.createEntityByIDFromName(testMonster, worldIn);
-//    			NBTTagCompound testTag = entity.serializeNBT();
-//    			
-//    			try
-//				{
-//					File path = File.createTempFile("test", ".json");
-//					path.setWritable(true);
-//					Files.write(testTag.toString(), path, Charset.defaultCharset());
-//					FMLLog.log.warn("Data Written to:" + path.getAbsolutePath());
-//				}
-//				catch (IOException e)
-//				{
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-//    		}
     		
     		if (usedItem != null && !usedItem.isEmpty() && usedItem.getItem() instanceof ItemTotemOfSpawning)
     		{
@@ -168,5 +202,101 @@ public class BlockAltarOfSpawning extends TileBlockBase<TileEntityAltarOfSpawnin
 	@Override
 	public void customBreakBlock(TileEntityAltarOfSpawning tileEntity, World worldIn, BlockPos pos, IBlockState state)
 	{
+	}
+	
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand)
+    {
+        double d0 = (double)pos.getX() + 0.5D;
+        double d1 = (double)pos.getY() + 1.05D;
+        double d2 = (double)pos.getZ() + 0.03D;
+
+        worldIn.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, d0, d1, d2, 0.0D, 0.0D, 0.0D, new int[0]);
+        worldIn.spawnParticle(EnumParticleTypes.FLAME, d0, d1, d2, 0.0D, 0.0D, 0.0D, new int[0]);
+        
+        d0 = (double)pos.getX() + 0.05D;
+        d2 = (double)pos.getZ() + 0.50D;
+        worldIn.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, d0, d1, d2, 0.0D, 0.0D, 0.0D, new int[0]);
+        worldIn.spawnParticle(EnumParticleTypes.FLAME, d0, d1, d2, 0.0D, 0.0D, 0.0D, new int[0]);
+        
+        d0 = (double)pos.getX() + 0.5D;
+        d2 = (double)pos.getZ() + .93D;
+        worldIn.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, d0, d1, d2, 0.0D, 0.0D, 0.0D, new int[0]);
+        worldIn.spawnParticle(EnumParticleTypes.FLAME, d0, d1, d2, 0.0D, 0.0D, 0.0D, new int[0]);
+    }
+	
+    /**
+     * A simple transparent material which does block movement.
+     * The core MaterialTransparent doesn't block movement.
+     * @author WuestMan
+     *
+     */
+	public static class SeeThroughMaterial extends Material
+	{
+
+		protected boolean translucent;
+		protected boolean blocksMovement;
+		
+		public SeeThroughMaterial(MapColor color)
+		{
+			super(color);
+			this.blocksMovement = true;
+		}
+		
+	    /**
+	     * Will prevent grass from growing on dirt underneath and kill any grass below it if it returns true
+	     */
+	    @Override
+		public boolean blocksLight()
+	    {
+	        return false;
+	    }
+	    
+	    public SeeThroughMaterial setTranslucent(boolean value)
+	    {
+	    	this.translucent = value;
+	    	return this;
+	    }
+	    
+	    /**
+	     * Indicate if the material is opaque
+	     */
+	    @Override
+	    public boolean isOpaque()
+	    {
+	        return this.translucent ? false : this.blocksMovement();
+	    }
+
+	    /**
+	     * Returns if this material is considered solid or not
+	     */
+	    @Override
+	    public boolean blocksMovement()
+	    {
+	        return this.blocksMovement;
+	    }
+	    
+	    /**
+	     * Sets the blocks movement field.
+	     * @param value The new value of the field.
+	     * @return This instance for property shortcuts.
+	     */
+	    public SeeThroughMaterial setBlocksMovement(boolean value)
+	    {
+	    	this.blocksMovement = value;
+	    	return this;
+	    }
+	    
+	    /**
+	     * Sets the immovable field.
+	     * @param value The new value of the field.
+	     * @return This instance for property shortcuts.
+	     */
+	    public SeeThroughMaterial setImmovable(boolean value)
+	    {
+	    	return (SeeThroughMaterial)this.setImmovableMobility();
+	    }
+		
 	}
 }
